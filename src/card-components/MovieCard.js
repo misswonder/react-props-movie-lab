64a2682e4117ecdb531b69f7f8b3f0d005062cba
Yvonne Chen <yvonnechen1991@gmail.com>
2021-03-15 17:27:35 -0500

    Done.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -17,3 +17,27 @@ export default class MovieShowcase extends Component {
 		return <div id="movie-showcase">{this.generateMovieCards()}</div>;
 	}
 }
+
+// import React, { Component } from 'react';
+// import CardFront from './CardFront.js';
+// import CardBack from './CardBack.js';
+// import defaultPoster from '../assets/the-trash-man.png'
+
+// export default class MovieCard extends Component {
+
+//   render() {
+//     return (
+//       <div className="movie-card">
+//         <CardFront posterURL={this.props.posterURL} />
+//         <CardBack title={this.props.title} IMDBRating={this.props.IMDBRating} genres={this.props.genres} />
+//       </div>
+//     )
+//   }
+// }
+
+// MovieCard.defaultProps = {
+//   title: "Poop",
+//   IMDBRating: 'No Rating Found',
+//   genres: ['No Genre(s) Found'],
+//   posterURL: defaultPoster
+// }
